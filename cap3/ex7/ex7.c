a849826931e0c7541f23f4d46212015d39fa5c8c
Lucas Amaral Taylor <87903796+lucasamtaylor01@users.noreply.github.com>
2022-09-16 13:39:30 -0300

    Deletando por erro de formatação

--- cap3/ex7/ex7.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main(){
-    float real, dolar;
-    printf("Insira o valor em reais: ");
-    scanf("%f", &real);
-    dolar = real/5.3; //cotação do dia 16.09.22
-    printf("R$ %f equivale a U$ %f\n", real, dolar);
-    return 0;
-}
