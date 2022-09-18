31f12492305af5203fe117a9465956d153714f9c
Lucas Amaral Taylor <87903796+lucasamtaylor01@users.noreply.github.com>
2022-09-18 16:34:45 -0300

    Delete cap_3 directory
    Organizando a formatação do diretório

--- cap_3/ex8/ex8.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main(){
-    float c, f;
-    printf("Insira a temperatura em Celsius: ");
-    scanf("%f", &c);
-    f = (c*9)/5 + 32;
-    printf("%f °C equivale a %f °F", c, f);
-    return 0;
-}
